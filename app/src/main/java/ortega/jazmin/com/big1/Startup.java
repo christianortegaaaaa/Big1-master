6ceaebf29cf23a8f5867feb9c5483a9e8e46b92d
Christian Ortega <2015081034@ust-ics.mygbiz.com>
2018-11-15 19:35:20 +0800

    v2

--- app/src/main/java/ortega/jazmin/com/big1/Startup.java
+++ app/src/main/java/ortega/jazmin/com/big1/Startup.java
@@ -1,16 +1,22 @@
 package ortega.jazmin.com.big1;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Startup extends AppCompatActivity {
-
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+        myDialog = new Dialog(this);
     }
     public void StartUp(View v) {
         Intent i = null, chooser = null;
@@ -26,4 +32,18 @@ public class Startup extends AppCompatActivity {
             startActivity(i);
         }
 }
+    public void ShowPopup(View v) {
+        TextView closebtn;
+        myDialog.setContentView(R.layout.activity_info_page);
+        closebtn =(TextView) myDialog.findViewById(R.id.closebtn);
+        closebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
+
     }
