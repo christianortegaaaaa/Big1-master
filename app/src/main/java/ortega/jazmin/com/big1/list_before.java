96ebc1059744cb4dff0d01ff0e6a34b2b7826741
Christian Ortega <2015081034@ust-ics.mygbiz.com>
2018-11-23 23:29:38 +0800

    v3

---
+++ app/src/main/java/ortega/jazmin/com/big1/list_before.java
@@ -0,0 +1,13 @@
+package ortega.jazmin.com.big1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class list_before extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_before);
+    }
+}
